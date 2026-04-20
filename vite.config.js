c9b7000da898ed632a2c7a8eb8e85c32bac4c5e5
POMMOUYLANG <mouylangpom@gmail.com>
2026-04-20 13:25:41 +0700

    chore: rename project, update base path, and add gh-pages deployment scripts

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Protfolio-2024/",
+  base: "/Protfolio/",
   plugins: [react()],
 });
